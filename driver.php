ba4351cc23fef74cee9bfb816c02ba6233e8d612
eswan18 <eswan18@rocketmail.com>
2017-01-11 17:50:26 -0500

    first commit

---
+++ driver.php
@@ -0,0 +1,7 @@
+<?php
+	require_once('libs/baker_street_boys.php');
+
+	use Baker_Street_Boys as bsb;
+
+	echo bsb\test(1);
+?>
---
+++ libs/baker_street_boys.php
@@ -0,0 +1,9 @@
+<?php
+	namespace Baker_Street_Boys;
+
+	function test($x) {
+		return($x + 1);
+	}
+
+	$bsb_flag = 1;
+?>
