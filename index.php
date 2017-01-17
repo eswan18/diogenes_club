771e72774a63af161a12e65fd25352466f28a6fe
Ethan Swan <eswan@Ethan-Swans-MBP.Home>
2017-01-16 20:25:01 -0500

    renamed index2 to index

--- index.php
+++ index.php
@@ -4,40 +4,50 @@
     <title>The Diogenes Club</title>
     <script src='http://code.jquery.com/jquery-latest.min.js'></script>
     <link rel='stylesheet' type='text/css' href='css/main.css'>
+    <?php
+      //Import framework and libraries
+      require_once('libs/Baker_Street_Boys.php');
+      use Baker_Street_Boys as bsb;
+      require_once('libs/General_Lib.php');
+      use General_Lib as lib;
+    ?>
   </head>
 
   <body>
 
-    <table class='content_table'>
-      <tr>
-	<td></td>
-	<td></td>
-	<td>settings and login icons</td>
-      </tr>
-      <tr>
-	<td></td>
-	<td><h1 class='page_title'>The Diogenes Club</h1></td>
-	<td></td>
-      </tr>
-      <tr>
-	<td></td>
-	<td>Search bar</td>
-	<td></td>
-      </tr>
-      <tr>
-	<td colspan='3'>
-	  <table class='bubble_table'>
-	    <tr>
-	      <td>Hot Topics</td>
-	      <td>Recently Perused</td>
-	    </tr>
-	    <tr>
-	      <td colspan='2'>Categories</td>
-	    </tr>
-	  </table>
-	</td>
-      </tr>
-    <table>
+    <?php
+      //create a table with 5 rows, with 3,3,3,2,and1 cols respectively
+      $content_table = new bsb\HTML_Table([3, 1, 3, 1, 2, 1], array("class" => content_table));
+      
+      $row = 0;
+      $content_table->set_html($row, 2, '<img id="settings_icon" src="assets/images/settings_icon.png">
+	      				<div id="user_label">USER</div>');
+      
+      $row = 1;
+      $content_table->set_html($row, 0, "<h1 class='page_title'>The Diogenes Club</h1>");
+
+      $row = 2;
+      $content_table->set_html($row, 1, '<form method="get" action="search.php"><span class="search"><input class="search" type="text" name="search" placeholder="Search for a topic"><span></form>');
+      
+      $row = 3;
+      $content_table->set_html($row, 0, '<div class="spacer" id="index_spacer_under_search"/>');
+      
+      $row = 4;
+      $content_table->set_html($row, 0, '<div class="content_bubble" id="hot_topics">
+		<h2>Hot Topics</h2>
+		<br><br><br><br><br>
+		</div>');
+      $content_table->set_html($row, 1, '<div class="content_bubble" id="recently_perused">
+		<h2>Recently Perused</h2>
+		<br><br><br><br><br>
+		</div>');
+      
+      $row = 5;
+      $content_table->set_html($row, 0, 'Categories');
+
+      //output the table's contents
+      echo $content_table->output();
+    ?>
 
   </body>
 </html>
