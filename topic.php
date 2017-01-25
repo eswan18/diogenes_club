bb174809f3b2b3baa7a6e7717b3c74335d505cb2
Ethan Swan <eswan18@rocketmail.com>
2017-01-25 13:01:33 -0500

    Fixed topic page. Added details

--- topic.php
+++ topic.php
@@ -17,14 +17,15 @@
 
     <?php
       //create a table with 4 rows
-      $content_table = new bsb\HTML_Table([3, 3, 1, 3], array("class" => content_table));
+      $content_table = new bsb\HTML_Table([3, 1, 3, 3], array("class" => content_table));
       
       $row = 0;
+      $content_table->set_html($row, 0, '<h2 class="site_name">The Diogenes Club</h2>');
       $content_table->set_html($row, 2, '<img id="settings_icon" src="assets/images/settings_icon.png">
 	      				<div id="user_label">USER</div>');
       
       $row = 1;
-      $content_table->set_html($row, 0, "<h1 class='page_title'>The Diogenes Club</h1>");
+      $content_table->set_html($row, 0, "<h1 class='page_title'>Topic</h1>");
 
       $row = 2;
       $content_table->set_html($row, 1, '<form method="get" action="search.php"><span class="search"><input class="search" type="text" name="search" placeholder="Search for a topic"><span></form>');
