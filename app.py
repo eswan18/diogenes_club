debb2867f43dfb0831787c5f85f05d7f872f2577
EC2 Default User <ec2-user@ip-172-31-33-60.us-east-2.compute.internal>
2017-02-25 18:48:26 +0000

    got functioning home page, started topic page

--- app.py
+++ app.py
@@ -9,6 +9,6 @@ def home():
 def user():
     return('User')
 
-@app.route('/topic/')
-def topic():
-    return('Topic')
+@app.route('/topic/<topic_name>/')
+def topic(topic_name=None):
+    return(render_template('topic.html', topic_name=topic_name))
