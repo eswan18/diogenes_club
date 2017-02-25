80a2ee869818d9352f3bf770cd5f8a5969e13c1f
Ethan Swan <ec2-user@ip-172-31-33-60.us-east-2.compute.internal>
2017-02-25 22:46:26 +0000

    created mysql function to get credentials

--- BakerStreetBoys/mysql.py
+++ BakerStreetBoys/mysql.py
@@ -2,6 +2,13 @@ import os
 
 def get_cred():
     cred_filename = "/home/ec2-user/.mysql.credentials"
+    creds = {}
     for line in open(cred_filename):
-        print line
-    return "hi"
+        elements = line.split(":")
+        if (len(elements) != 2):
+            raise Exception("mysql credentials file not formatted correctly")
+        key = elements[0].strip()
+        value = elements[1].strip()
+        creds[key] = value
+    print(creds)
+    return creds
