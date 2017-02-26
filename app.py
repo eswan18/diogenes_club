90912bcb4b0abed54710dfd222e7bd77aca078fb
Ethan Swan <ec2-user@ip-172-31-33-60.us-east-2.compute.internal>
2017-02-26 17:12:14 +0000

    established connection to mysql

--- app.py
+++ app.py
@@ -1,13 +1,28 @@
-from flask import Flask, render_template
-app = Flask(__name__)
+#from flask import Flask, render_template
+import flask
+import flask_mysqldb
 import BakerStreetBoys as bsb
 
 # Load our mysql credentials
 mysql_cred = bsb.mysql.get_cred()
 
+# Initialize
+app = flask.Flask(__name__)
+# Config Directives
+app.config["MYSQL_USER"] = mysql_cred["user"]
+app.config["MYSQL_PASSWORD"] = mysql_cred["password"]
+app.config["MYSQL_DB"] = mysql_cred["database"]
+app.config["MYSQL_HOST"] = mysql_cred["host"]
+
+mysql = flask_mysqldb.MySQL(app)
+
 @app.route('/')
 def home():
-    return(render_template('home.html'))
+    cur = mysql.connection.cursor()
+    cur.execute('''SELECT * FROM users''')
+    rv = cur.fetchall()
+    print(str(rv))
+    return(flask.render_template('home.html'))
 
 @app.route('/user/')
 def user():
@@ -15,8 +30,8 @@ def user():
 
 @app.route('/topic/<topic_name>/')
 def topic(topic_name=None):
-    return(render_template('topic.html', topic_name=topic_name.upper()))
+    return(flask.render_template('topic.html', topic_name=topic_name.upper()))
 
 @app.route('/search/<query>/')
 def search(query=None):
-    return(render_template('search.html', query=query.lower()))
+    return(flask.render_template('search.html', query=query.lower()))
