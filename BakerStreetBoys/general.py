2e25a48cdd93e759e62c6687363a5d028451c493
Ethan Swan <ec2-user@ip-172-31-33-60.us-east-2.compute.internal>
2017-02-27 01:32:01 +0000

    began work on logging in

---
+++ BakerStreetBoys/general.py
@@ -0,0 +1,13 @@
+import os
+
+def get_secret_key():
+    key_filename = "/home/ec2-user/.dc.keys"
+    keys = {}
+    for line in open(key_filename):
+        elements = line.split(":")
+        if (len(elements) != 2):
+            raise Exception("secret key file not formatted correctly")
+        key = elements[0].strip()
+        value = elements[1].strip()
+        keys[key] = value
+    return keys
--- app.py
+++ app.py
@@ -1,18 +1,19 @@
-#from flask import Flask, render_template
-import flask
-import flask_mysqldb
+from flask import Flask, render_template, request, session, redirect, url_for
+import flask_mysqldb, werkzeug.security
 import BakerStreetBoys as bsb
 
-# Load our mysql credentials
+# Load our credentials
 mysql_cred = bsb.mysql.get_cred()
+secret_key = bsb.general.get_secret_key()["secret_key"]
 
 # Initialize
-app = flask.Flask(__name__)
+app = Flask(__name__)
 # Config Directives
 app.config["MYSQL_USER"] = mysql_cred["user"]
 app.config["MYSQL_PASSWORD"] = mysql_cred["password"]
 app.config["MYSQL_DB"] = mysql_cred["database"]
 app.config["MYSQL_HOST"] = mysql_cred["host"]
+app.secret_key = secret_key
 
 mysql = flask_mysqldb.MySQL(app)
 
@@ -22,7 +23,7 @@ def home():
     cur.execute('SELECT * FROM topics')
     raw_topics = cur.fetchall()
     topics = [x[1] for x in raw_topics]
-    return(flask.render_template('home.html',
+    return(render_template('home.html',
                                  topics = topics))
 
 @app.route('/user/')
@@ -31,8 +32,38 @@ def user():
 
 @app.route('/topic/<topic_name>/')
 def topic(topic_name=None):
-    return(flask.render_template('topic.html', topic_name=topic_name.upper()))
+    return(render_template('topic.html', topic_name=topic_name.upper()))
 
 @app.route('/search/<query>/')
 def search(query=None):
-    return(flask.render_template('search.html', query=query.lower()))
+    return(render_template('search.html', query=query.lower()))
+
+@app.route('/login/', methods=['GET','POST'])
+def login():
+    # Post requests indicate a login attempt
+    if (request.method == 'POST'):
+        # If username and password aren't both set, then it's a bad request
+        if ('username' not in request.form or 'password' not in request.form):
+            return('Bad request - you must send a uname and pw')
+        username = request.form['username']
+        password = request.form['password']
+        hashed_password = werkzeug.security.generate_password_hash(password)
+        print(username, password, hashed_password)
+        # Verify them against the database
+        
+        # Then set the session username
+        session['username'] = request.form['username']
+        #session['user_id'] = user_id
+    # If the user is already logged in
+    return(render_template('login_unauth.html'))
+    if ('username' in session):
+        return("You're logged in as " + session['username'])
+    else:
+        return(render_template('login_unauth.html'))
+        
+
+@app.route('/logout/')
+def logout():
+    session.pop('username', None)
+    return(redirect(url_for('home')))
+    
